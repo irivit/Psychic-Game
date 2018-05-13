95efdd697cfaf56cfbf4ff7c9afc442add43efe4
irivi <copacies@gmail.com>
2018-05-13 14:21:12 -0500

    Game finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,54 +2,58 @@
 
 var win = 0;         // variable for amount of time that user wins
 var losses =0;         // variable for amount of time that user lost
-var guessesLeft =  9; // variable for the amunt of word that the user left to guest
+var guessesLeft = 9; // variable for the amunt of word that the user left to guest
 var myLetter = ''; // variable to catch my letters
 var letterSoFar = new Array();
 
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
 
 var compLetter = alphabet[Math.floor(Math.random() * alphabet.length)]; //generate a random letter
-console.log(compLetter);
+
 
 
 var reset = function(){
-    guessesLeft = 9;
-    letterSoFar = [];
+    guessesLeft = 9; //reset guesses lef
+    letterSoFar = []; // reste the array
 
-    updateLetterSoFar();
-    document.getElementById("left").innerHTML = guessesLeft;
+    updateLetterSoFar(); //show the array so far
+    updateGuessesLeft(); //update guesses left
     
-    compLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-
-    console.log(compLetter);
-
+    compLetter = alphabet[Math.floor(Math.random() * alphabet.length)]; //generate a new letter from my alphabet
 }
 
+
+// this function update the letter pressed by the user
 function updateLetterSoFar(){
     document.getElementById("letters").innerHTML = letterSoFar.join(', ');
 }  
 
+
+//this function update the guesses letters so far
+function updateGuessesLeft(){
+    document.getElementById("left").innerHTML = guessesLeft; 
+}
+
+
+
 document.onkeyup =function(event){
-    guessesLeft--;
+    guessesLeft--; 
     var myLetter = String.fromCharCode(event.keyCode).toLowerCase(); // store the letter pressed by user
 
  
-    letterSoFar.push(myLetter);
-    updateLetterSoFar();
+    letterSoFar.push(myLetter); //add a new letter to the array of letter pressed by user
+    updateLetterSoFar(); //call function to update the array
       
-    
-    if (guessesLeft > 0){
-
-        if (compLetter === myLetter){
-            win++;
-            document.getElementById("win").innerHTML = win;
-            reset(); 
-        }
-    }else  {
-        losses++;
-        document.getElementById("loss").innerHTML = losses;        
-        reset();
+    if (compLetter === myLetter){ //compare if the first letter pressed is equal to the letter generated from alphabet
+        win++;  
+        document.getElementById("win").innerHTML = win; //update the win
+        reset();  // call the function to clean my variables
+    }else if(guessesLeft > 0){ //if the letter are differents ask for my left oportunities
+        updateGuessesLeft(); //call the function to update left oportunities 
+    }else{
+        losses++; //increment the variables losses
+        document.getElementById("loss").innerHTML = losses; //update the losses on html  
+        reset(); // call the function to clean my variables
     }
-
 
 }
